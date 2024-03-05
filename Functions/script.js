88ff2c66fe4bc9909cea4f6248d4c7c79deaed10
Megha Mathur <meghamathur2001@gmail.com>
2024-03-05 17:19:39 +0530

    Higher order functions

--- Functions/script.js
+++ Functions/script.js
@@ -21,3 +21,27 @@ createBooking('LH123', 2);
 createBooking('LH123', 5);
 
 createBooking('LH123', undefined, 1000);
+
+
+//*****************************call back functions****************** */
+
+const oneWord = function (str) {
+    return str.replace(/ /g, '').toLowerCase();
+  };
+  
+  const upperFirstWord = function (str) {
+    const [first, ...others] = str.split(' ');
+    return [first.toUpperCase(), ...others].join(' ');
+  };
+  
+  // Higher-order function
+  const transformer = function (str, fn) {
+    console.log(`Original string: ${str}`);
+    console.log(`Transformed string: ${fn(str)}`);
+  
+    console.log(`Transformed by: ${fn.name}`);
+  };
+  
+  transformer('JavaScript is the best!', upperFirstWord);
+  transformer('JavaScript is the best!', oneWord);
+  
