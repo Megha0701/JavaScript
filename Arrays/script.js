7e66f66d37d2e7168e176c88eea78e88eb98f00b
Megha Mathur <meghamathur2001@gmail.com>
2024-03-05 15:31:55 +0530

    Final commit for Array methods

--- Arrays/script.js
+++ Arrays/script.js
@@ -51,17 +51,17 @@
 
 //**************at Method***********
 
-const arr = [23, 11, 64];
-console.log(arr[0]);
-console.log(arr.at(0));
+// const arr = [23, 11, 64];
+// console.log(arr[0]);
+// console.log(arr.at(0));
 
-//grtting last array element
-console.log(arr[arr.length-1]);
-console.log(arr.slice(-1)[0]);
-console.log(arr.at(-1));
+// //grtting last array element
+// console.log(arr[arr.length-1]);
+// console.log(arr.slice(-1)[0]);
+// console.log(arr.at(-1));
 
-console.log('jonas'.at(0));
-console.log('jonas'.at(-1));
+// console.log('jonas'.at(0));
+// console.log('jonas'.at(-1));
 
 // Looping Arrays: forEach
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
@@ -85,7 +85,7 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 // });
 
 
-//***********filter************
+//***********filter************returns new array
 const deposits = movements.filter(function (mov) {
     return mov > 0;
   });
@@ -94,6 +94,8 @@ const deposits = movements.filter(function (mov) {
     
   const withdrawals = movements.filter(mov => mov < 0);
   console.log(withdrawals);
+
+
 
 
 
@@ -107,3 +109,74 @@ console.log(movements);
 const balance = movements.reduce((acc, cur) => acc + cur, 0);
 console.log(balance);
 
+
+
+
+
+//******************find*************** returns first elemengt matching the logic
+
+const firstWithdrawal = movements.find(mov => mov < 0);
+console.log(movements);
+console.log(firstWithdrawal);
+
+
+
+
+
+
+//*****************SOME ans EVERY********* */
+
+console.log(movements);
+
+// EQUALITY
+console.log(movements.includes(-130));
+
+// SOME: CONDITION
+console.log(movements.some(mov => mov === -130));
+
+const anyDeposits = movements.some(mov => mov > 0);
+console.log(anyDeposits);
+
+// EVERY
+console.log(movements.every(mov => mov > 0));
+
+
+
+
+//*****************FLAT and FLATMAP******************* 
+
+const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+console.log(arr.flat());
+
+const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+console.log(arrDeep.flat(2));
+
+
+
+//********************Sorting**************** 
+// Strings
+const owners = ['Jonas', 'Zach', 'Adam', 'Martha'];
+console.log(owners.sort());
+console.log(owners);
+
+// Numbers
+console.log(movements);
+
+// return < 0, A, B (keep order)
+// return > 0, B, A (switch order)
+
+// Ascending
+// movements.sort((a, b) => {
+//   if (a > b) return 1;
+//   if (a < b) return -1;
+// });
+movements.sort((a, b) => a - b);
+console.log(movements);
+
+// Descending
+// movements.sort((a, b) => {
+//   if (a > b) return -1;
+//   if (a < b) return 1;
+// });
+movements.sort((a, b) => b - a);
+console.log(movements);
