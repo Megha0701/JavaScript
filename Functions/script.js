fc1dab8bb74a193ff04de7a377cd071d610f4aba
Megha Mathur <meghamathur2001@gmail.com>
2024-03-05 17:29:56 +0530

    functions returning functions

--- Functions/script.js
+++ Functions/script.js
@@ -45,3 +45,22 @@ const oneWord = function (str) {
   transformer('JavaScript is the best!', upperFirstWord);
   transformer('JavaScript is the best!', oneWord);
   
+
+
+  //*************Functions returning functions**************
+  const greet = function (greeting) {
+    return function (name) {
+      console.log(`${greeting} ${name}`);
+    };
+  };
+  
+  const greeterHey = greet('Hey');
+  greeterHey('Jonas');
+  greeterHey('Steven');
+  
+  greet('Hello')('Jonas');
+  
+  // Challenge
+  const greetArr = greeting => name => console.log(`${greeting} ${name}`);
+  
+  greetArr('Hi')('Jonas');
